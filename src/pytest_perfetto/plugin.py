cfcfe85b2cca654fe281f695ff328487994eb54d
Luca Muscat <lucamuscat@gmail.com>
2024-11-10 20:08:11 +0100

    Measure the time taken in the collection phase of the test

--- src/pytest_perfetto/plugin.py
+++ src/pytest_perfetto/plugin.py
@@ -1,4 +1,5 @@
-from typing import Generator
+from time import time
+from typing import Any, Generator, Optional
 
 import pytest
 
@@ -7,3 +8,14 @@ import pytest
 def pytest_pyfunc_call() -> Generator[None, None, None]:
     print("Hello world")
     yield
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_collection() -> Generator[Optional[object], None, None]:
+    start = time()
+
+    result = yield None
+    print(f"Time taken collecting tests: {time() - start}")
+    return result
+
+
