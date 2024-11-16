b81de35b603690d98175c15318d42b4792b71295
Luca Muscat <lucamuscat@gmail.com>
2024-11-16 12:58:21 +0100

    Introduce `DurationEvent`s

--- src/pytest_perfetto/plugin.py
+++ src/pytest_perfetto/plugin.py
@@ -3,10 +3,15 @@ The pytest-perfetto plugin aims to help developers profile their tests by ultima
 'perfetto' trace file, which may be natively visualized using most Chromium-based browsers.
 """
 
+from dataclasses import dataclass
+from enum import Enum
 from time import time
-from typing import Any, Generator, Optional
+from typing import Any, Dict, Generator, Literal, NewType, Optional
 
 import pytest
+
+Category = NewType("Category", str)
+Timestamp = NewType("Timestamp", int)
 
 
 class TraceEvent:
@@ -16,6 +21,49 @@ class TraceEvent:
     """
 
     ...
+
+
+class Phase(str, Enum):
+    """
+    The phase described the event type. This is a single character which changes depending on the
+    type of event being output. This class will encapsulate all the valid phase types
+
+    The following is a description of some of the valid event types:
+
+    * Duration Events:
+        Duration events provide a way to mark a duration of work on a given thread. Duration events
+        are marked by the 'B' & 'E' phase types. The 'B' event must come before the 'E' event. 'B' &
+        'E' events may be nested, allowing the capturing of function calling behaviour on a thread.
+        The timestamps for duration events must be in an increasing order for a given thread.
+        Timestamps in different threads do not have to be in increasing order.
+    """
+
+    B = "B"
+    """Marks the beginning of a duration event."""
+    E = "E"
+    """Marks the end of a duration event"""
+
+
+class DurationEvent: ...
+
+
+@dataclass(frozen=True)
+class BeginDurationEvent(DurationEvent):
+    name: str
+    cat: Category
+    ts: Timestamp
+    pid: int
+    tid: int
+    args: Dict[str, Any]
+    ph: Literal[Phase.B] = Phase.B
+
+
+@dataclass(frozen=True)
+class EndDurationEvent(DurationEvent):
+    pid: int
+    tid: int
+    ts: Timestamp
+    ph: Literal[Phase.E] = Phase.E
 
 
 @pytest.hookimpl(hookwrapper=True)
