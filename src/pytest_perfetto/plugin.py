632d5f33bcd03f4e939d3c04eb4b234aa398e7cd
Luca Muscat <lucamuscat@gmail.com>
2024-11-21 19:59:31 +0100

    Fix grammar in code comments

--- src/pytest_perfetto/plugin.py
+++ src/pytest_perfetto/plugin.py
@@ -18,7 +18,7 @@ Timestamp = NewType("Timestamp", float)
 
 class TraceEvent:
     """
-    The Trace Event Format is the trace data representation that is processed byt he Trace
+    The Trace Event Format is the trace data representation that is processed by the Trace
     Viewer. [This document is the canonical reference on the 'Trace Event Format'](https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview?tab=t.0)
     """
 
@@ -27,27 +27,28 @@ class TraceEvent:
 
 class Phase(str, Enum):
     """
-    The phase described the event type. This is a single character which changes depending on the
-    type of event being output. This class will encapsulate all the valid phase types
+    The phase describes the event's type. The phase is a single character which changes depending on
+    the type of event. This class encapsulates all the valid phase types.
 
     The following is a description of some of the valid event types:
 
     * Duration Events:
-        Duration events provide a way to mark a duration of work on a given thread. Duration events
-        are marked by the 'B' & 'E' phase types. The 'B' event must come before the 'E' event. 'B' &
-        'E' events may be nested, allowing the capturing of function calling behaviour on a thread.
-        The timestamps for duration events must be in an increasing order for a given thread.
-        Timestamps in different threads do not have to be in increasing order.
+        Duration events provide a way of marking a duration of work on a given thread. Duration
+        events are marked by the 'B' & 'E' phase types. The 'B' event must come before the 'E'
+        event. 'B' & 'E' events may be nested, allowing the capturing of function calling behaviour
+        on a thread. The timestamps for duration events must be in an increasing order for a given
+        thread. Timestamps in different threads do not have to be in increasing order.
     * Instant Events:
-        Instant events correspond to something that happens but has no duration associated with it.
+        Instant events, or points in time, that correspond to an event that happens but has no
+        associated duration.
     """
 
     B = "B"
     """Marks the beginning of a duration event."""
     E = "E"
-    """Marks the end of a duration event"""
+    """Marks the end of a duration event."""
     i = "i"
-    """Marks an instant event"""
+    """Marks an instant event."""
 
 
 @dataclass(frozen=True)
@@ -78,7 +79,7 @@ class InstantScope(str, Enum):
     the instant even in Trace Viewer."""
 
     g = "g"
-    """Global scoped instant event. A global scoped event will draw a line from the top to the
+    """Global scoped instant event. A globally scoped event will draw a line from the top to the
     bottom of the timeline."""
     p = "p"
     """Process scoped instant event. A process scoped instant event will draw through all threads of
