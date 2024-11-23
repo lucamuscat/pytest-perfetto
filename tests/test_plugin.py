86e6dbde4308bb841d6c88a71a62d6fb9ae30a31
Luca Muscat <lucamuscat@gmail.com>
2024-11-23 13:14:12 +0100

    Only write the trace file if the `--perfetto` arg is provided

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,1 +1,16 @@
-def test_hello() -> None: ...
+from pathlib import Path
+from uuid import uuid4
+
+import pytest
+
+
+def test_given_perfetto_arg_trace_files_are_written(
+    pytester: pytest.Pytester, tmp_path: Path
+) -> None:
+    perfetto_arg_value: Path = tmp_path / f"{uuid4().hex}.json"
+    pytester.makepyfile("""
+        def test_hello(): ...
+    """)
+    result = pytester.runpytest_subprocess(f"--perfetto={perfetto_arg_value}")
+    result.assert_outcomes(passed=1)
+    assert perfetto_arg_value.exists()
